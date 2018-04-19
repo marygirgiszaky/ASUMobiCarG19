b3e033441cdf55b11af050b6a8105d5abb35b786
marygirgiszaky <36311441+marygirgiszaky@users.noreply.github.com>
2018-04-19 22:40:17 +0200

    variabie resistance

---
+++ variable_resistor.ino
@@ -0,0 +1,27 @@
+void setup() {
+  int ledpin =9;
+  int analogpin = 0;
+}
+
+void loop() {
+  for (int i=0 ; i<=255 ; i++)
+{ 
+  analogWrite( ledpin , i);
+  delay(delayval());
+  
+}
+ for (int i=225 ; i>0 ; i--)
+{ 
+  analogWrite( ledpin , i);
+  delay(delayval());
+  
+}
+int delayval()
+{
+  int v ;
+  v = analogRead (analogpin);
+  v /= 8;
+  return v;
+  
+}
+}
